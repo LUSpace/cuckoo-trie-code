a6ab80bb6ec230810450ffe6e534915ea0ad74d0
Baotong LU <lubaotong22@gmail.com>
2021-12-15 15:16:58 +0800

    fix

--- cc_wrapper.cpp
+++ cc_wrapper.cpp
@@ -1,5 +1,5 @@
 #include "cc_wrapper.hpp"
 
-extern "C" void *create_tree() {
+extern "C" __attribute__((visibility("default"))) void *create_tree() {
   return reinterpret_cast<void *>(new cc_wrapper<string_key *, char *>());
 }
